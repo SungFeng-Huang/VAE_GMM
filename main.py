6c8e3a782fd5466a1c04a287d7aca80a4ec37e2e
b02901071 <b02901071@ntu.edu.tw>
2017-03-27 00:30:15 +0800

    gif

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ Reference: "Auto-Encoding Variational Bayes" https://arxiv.org/abs/1312.6114
 '''
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 from scipy.stats import norm
+import math
 
 from keras.layers import Input, Dense, Lambda, Dropout
 from keras.models import Model
@@ -14,10 +16,12 @@ from keras.datasets import mnist
 from lib import generateGMMs
 #from matplotlib.pylab import *
 
-n_gauss = 4
-n_latent = 16
-n_hidden_units = 512
-n_layer = 6
+#####
+# settings
+n_gauss = 2
+n_latent = 2
+n_hidden = 256
+n_layer = 3
 nb_epoch = 500
 
 n_dim = 2
@@ -27,10 +31,12 @@ n_samples_test = 1000
 batch_size = 100
 original_dim = n_dim
 latent_dim = n_latent
-intermediate_dim = n_hidden_units
+intermediate_dim = n_hidden
 epsilon_std = 1e-3
 
 
+#####
+# build model
 x = Input(batch_shape=(batch_size, original_dim))
 h = x
 for i in range(n_layer):
@@ -45,10 +51,8 @@ def sampling(args):
                               std=epsilon_std)
     return z_mean + K.exp(z_log_var / 2) * epsilon
 
-# note that "output_shape" isn't necessary with the TensorFlow backend
 z = Lambda(sampling, output_shape=(latent_dim,))([z_mean, z_log_var])
 
-# we instantiate these layers separately so as to reuse them later
 decoder_h = []
 for i in range(n_layer):
     decoder_h.append(Dense(intermediate_dim, activation='relu', W_regularizer='l1l2'))
@@ -68,6 +72,8 @@ vae = Model(x, x_decoded_mean)
 vae.compile(optimizer='rmsprop', loss=vae_loss)
 vae.summary()
 
+
+#####
 # make training/testing datas
 print('Generating data, takes around 25 secs')
 
@@ -78,6 +84,97 @@ GMM = GMMs_train[0]
 mean, var, weight, samples, pdf = GMM.mean, GMM.var, GMM.weight, GMM.samples, GMM.pdf
 x_train = samples[:9000]
 x_valid = samples[9000:]
+
+
+#####
+# build a generator
+decoder_input = Input(shape=(latent_dim,))
+_h_decoded = decoder_input
+for i in range(n_layer):
+    _h_decoded = decoder_h[i](_h_decoded)
+_x_decoded_mean = decoder_mean(_h_decoded)
+generator = Model(decoder_input, _x_decoded_mean)
+
+
+#####
+# generate latent samples
+grid = np.random.multivariate_normal(np.zeros(latent_dim), np.eye(latent_dim), 1000)
+
+#####
+# animation
+plt.axis((-12, 10, -12, 8))
+fig, ax = plt.subplots()
+fig.set_tight_layout(True)
+
+ax.scatter(x_valid[:, 0], x_valid[:, 1], color='green')
+x_gen = []
+for z in grid:
+    z_sample = np.array([z])
+    x_decoded = generator.predict(z_sample)
+    x_gen.append(x_decoded)
+
+x_decoded = np.concatenate(x_gen)
+decode = ax.scatter(x_decoded[:, 0], x_decoded[:, 1], color='blue')
+
+
+def update(i):
+    # Iteration i
+    print('Iteration ' + str(i+1) + '/' + str(int(math.floor(nb_epoch/5.0))))
+
+    # train vae
+    vae.fit(x_train, x_train,
+            shuffle=True,
+            nb_epoch=5,
+            batch_size=batch_size,
+            validation_data=(x_valid, x_valid))
+
+    x_gen = []
+
+    for z in grid:
+        z_sample = np.array([z])
+        x_decoded = generator.predict(z_sample)
+        x_gen.append(x_decoded)
+
+    x_decoded = np.concatenate(x_gen)
+    decode.set_offsets(x_decoded)
+
+anim = FuncAnimation(fig, update, frames=np.arange(1, int(math.floor(nb_epoch/5.0))), interval=50)
+
+gifname = 'image/' + str(n_gauss) + '_' +  str(n_hidden) + '*' + str(n_layer) + '+' + \
+                str(n_latent) + '_' + str(epsilon_std) + '.gif'
+anim.save(gifname, dpi=fig.get_dpi(), writer='imagemagick')
+
+'''
+for i in range(int(math.floor(nb_epoch/5.0))):
+    # Iteration i
+    print('Iteration ' + str(i))
+
+    # train vae
+    vae.fit(x_train, x_train,
+            shuffle=True,
+            nb_epoch=5,
+            batch_size=batch_size,
+            validation_data=(x_valid, x_valid))
+
+    x_gen = []
+
+    for z in grid:
+        z_sample = np.array([z])
+        x_decoded = generator.predict(z_sample)
+        x_gen.append(x_decoded)
+
+    x_decoded = np.concatenate(x_gen)
+    plt.figure(figsize=(6, 6))
+    plt.scatter(x_decoded[:, 0], x_decoded[:, 1])
+    ###plt.colorbar()
+    imgname = 'image/' + str(n_gauss) + '_' +  str(n_hidden) + '*' + str(n_layer) + '+' + \
+                str(n_latent) + '_' + str(epsilon_std) + '_epoch_' + str((i+1) * 5) + '.png'
+    #plt.show()
+    print('Save img\n')
+    plt.axis((-12, 10, -12, 8))
+    plt.savefig(imgname)
+'''
+
 '''
 np.random.seed(113)
 GMMs_test = generateGMMs(n_gauss, n_dim, n_samples_test)
@@ -86,13 +183,6 @@ GMM = GMMs_test[0]
 mean, var, weight, samples, pdf = GMM.mean, GMM.var, GMM.weight, GMM.samples, GMM.pdf
 x_test = samples
 '''
-
-# train vae
-vae.fit(x_train, x_train,
-        shuffle=True,
-        nb_epoch=nb_epoch,
-        batch_size=batch_size,
-        validation_data=(x_valid, x_valid))
 
 '''
 # build a model to project inputs on the latent space
@@ -105,31 +195,6 @@ plt.scatter(x_valid_encoded[:, 0], x_valid_encoded[:, 1])
 ###plt.colorbar()
 plt.show()
 '''
-
-#'''
-# build a digit generator that can sample from the learned distribution
-decoder_input = Input(shape=(latent_dim,))
-_h_decoded = decoder_input
-for i in range(n_layer):
-    _h_decoded = decoder_h[i](_h_decoded)
-_x_decoded_mean = decoder_mean(_h_decoded)
-generator = Model(decoder_input, _x_decoded_mean)
-
-grid = np.random.multivariate_normal(np.zeros(latent_dim), np.eye(latent_dim), 1000)
-x_gen = []
-
-for z in grid:
-    z_sample = np.array([z])
-    x_decoded = generator.predict(z_sample)
-    x_gen.append(x_decoded)
-
-x_decoded = np.concatenate(x_gen)
-plt.figure(figsize=(6, 6))
-plt.scatter(x_decoded[:, 0], x_decoded[:, 1])
-###plt.colorbar()
-imgname = 'image\/' +str(n_gauss) + '_' +  str(n_hidden) + '*' + str(n_layer) + '+' + str(n_latent) + '_' + str(epsilon_std) + '.png'
-plt.savefig(imgname)
-#'''
 
 '''
 ######
